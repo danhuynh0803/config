fb7bc327222c667ae4af5142b8d58c95b0cb2e53
danhuynh0803 <danhuynh0803@gmail.com>
2021-05-22 15:45:52 -0700

    Repo alias on c drive

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 alias grep='grep --color'
 alias ls='ls -a --color'
+alias repos='cd /mnt/c/Users/OWNER/Repos/'
 
 set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.hg/*"'
 set -x FZF_DEFAULT_OPTS '--bind ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all'
